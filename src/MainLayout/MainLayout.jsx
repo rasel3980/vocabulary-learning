c23990770a6fb9607fe79faa18f100dd6dfad8b8
rasel3980 <rkrasel3980@gmail.com>
2024-11-17 21:55:46 +0600

    Added a React router

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 
 const MainLayout = () => {
     return (
         <div>
             Welcome , i am Mainlayout
+            <Outlet></Outlet>
         </div>
     );
 };
