7a1ad030c93bbbf0a777fdfb5c28d4fdb0ef914e
rasel3980 <rkrasel3980@gmail.com>
2024-11-18 15:43:13 +0600

    add footer section

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <li><NavLink to="/about us">About-Us</NavLink></li>
     </>
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 w-11/12 mx-auto">
       <div className="navbar-start">
         <div className="dropdown">
           <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Components/Header';
+import Footer from '../Footer/Footer';
 
 const MainLayout = () => {
     return (
-        <div className='w-11/12 mx-auto'>
+        <div>
             <Header></Header>
+            <div className='min-h-[calc(100vh-288px)]'>
             <Outlet></Outlet>
+            </div>
+            <Footer></Footer>
         </div>
     );
 };
